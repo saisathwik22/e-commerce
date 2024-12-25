49a137f056c6717ab7d4b4b6a0e3fa977b7c14f1
saisathwik22 <reddysaisathwik022@gmail.com>
2024-12-25 23:30:33 +0530

    User model setup, Password compare and hashing done. CodeStage - 31:35

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 // Routes imports
 import authRoutes from "./routes/auth.route.js";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
+
+  connectDB();
 });
