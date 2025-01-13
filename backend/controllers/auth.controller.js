840a4a894edcf3b4df7acd2ae0d86559dfdcd309
saisathwik22 <reddysaisathwik022@gmail.com>
2025-01-13 22:23:25 +0530

    generateTokens(access & refresh) function added. CodeStage - 52:00

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,27 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  ); // 7 days
+};
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -12,6 +35,10 @@ export const signup = async (req, res) => {
     const user = await User.create({ name, email, password });
 
     // Authenticate user
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    // setCookies(res, accessToken, refreshToken);
 
     res.status(201).json({ user, message: "User created successfully" });
   } catch (error) {
