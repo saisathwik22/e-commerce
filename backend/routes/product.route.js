7bd9cbe66afc933fa1300b7db106b9fc96bdd0cc
saisathwik22 <reddysaisathwik022@gmail.com>
2025-02-01 21:56:01 +0530

    CRUD code CodeStage - 1:53:00

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  createProduct,
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
